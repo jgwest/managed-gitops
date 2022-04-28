6876db17975e7caa886399d2c3c17b9bce097459
Tirth Mehta <tirth.mehta012@gmail.com>
2022-04-28 14:41:52 +0530

    Migrated Unit Tests to Ginkgo

--- backend-shared/config/db/clusteraccess_test.go
+++ backend-shared/config/db/clusteraccess_test.go
@@ -1,100 +1,90 @@
-package db
+package db_test
 
 import (
 	"context"
-	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	db "github.com/redhat-appstudio/managed-gitops/backend-shared/config/db"
 )
 
-func TestClusterAccessFunctions(t *testing.T) {
-	testSetup(t)
-	defer testTeardown(t)
-	dbq, err := NewUnsafePostgresDBQueries(true, true)
-	if !assert.NoError(t, err) {
-		return
-	}
-	defer dbq.CloseDatabase()
+var _ = Describe("ClusterAccess Tests", func() {
+	Context("It should execute all DB functions for ClusterAccess", func() {
+		It("Should execute all ClusterAccess Functions", func() {
+			ginkgoTestSetup()
+			dbq, err := db.NewUnsafePostgresDBQueries(true, true)
+			Expect(err).To(BeNil())
+			defer dbq.CloseDatabase()
 
-	ctx := context.Background()
+			ctx := context.Background()
 
-	var clusterUser = &ClusterUser{
-		Clusteruser_id: "test-user-application",
-		User_name:      "test-user-application",
-	}
-	err = dbq.CreateClusterUser(ctx, clusterUser)
-	if !assert.NoError(t, err) {
-		return
-	}
+			var clusterUser = &db.ClusterUser{
+				Clusteruser_id: "test-user-application",
+				User_name:      "test-user-application",
+			}
+			err = dbq.CreateClusterUser(ctx, clusterUser)
+			Expect(err).To(BeNil())
 
-	clusterCredentials := ClusterCredentials{
-		Clustercredentials_cred_id:  "test-cluster-creds-test-5",
-		Host:                        "host",
-		Kube_config:                 "kube-config",
-		Kube_config_context:         "kube-config-context",
-		Serviceaccount_bearer_token: "serviceaccount_bearer_token",
-		Serviceaccount_ns:           "Serviceaccount_ns",
-	}
+			clusterCredentials := db.ClusterCredentials{
+				Clustercredentials_cred_id:  "test-cluster-creds-test-5",
+				Host:                        "host",
+				Kube_config:                 "kube-config",
+				Kube_config_context:         "kube-config-context",
+				Serviceaccount_bearer_token: "serviceaccount_bearer_token",
+				Serviceaccount_ns:           "Serviceaccount_ns",
+			}
 
-	managedEnvironment := ManagedEnvironment{
-		Managedenvironment_id: "test-managed-env-5",
-		Clustercredentials_id: clusterCredentials.Clustercredentials_cred_id,
-		Name:                  "my env",
-	}
+			managedEnvironment := db.ManagedEnvironment{
+				Managedenvironment_id: "test-managed-env-5",
+				Clustercredentials_id: clusterCredentials.Clustercredentials_cred_id,
+				Name:                  "my env",
+			}
 
-	gitopsEngineCluster := GitopsEngineCluster{
-		Gitopsenginecluster_id: "test-fake-cluster-5",
-		Clustercredentials_id:  clusterCredentials.Clustercredentials_cred_id,
-	}
+			gitopsEngineCluster := db.GitopsEngineCluster{
+				Gitopsenginecluster_id: "test-fake-cluster-5",
+				Clustercredentials_id:  clusterCredentials.Clustercredentials_cred_id,
+			}
 
-	gitopsEngineInstance := GitopsEngineInstance{
-		Gitopsengineinstance_id: "test-fake-engine-instance-id",
-		Namespace_name:          "test-fake-namespace",
-		Namespace_uid:           "test-fake-namespace-5",
-		EngineCluster_id:        gitopsEngineCluster.Gitopsenginecluster_id,
-	}
+			gitopsEngineInstance := db.GitopsEngineInstance{
+				Gitopsengineinstance_id: "test-fake-engine-instance-id",
+				Namespace_name:          "test-fake-namespace",
+				Namespace_uid:           "test-fake-namespace-5",
+				EngineCluster_id:        gitopsEngineCluster.Gitopsenginecluster_id,
+			}
 
-	clusterAccess := ClusterAccess{
-		Clusteraccess_user_id:                   clusterUser.Clusteruser_id,
-		Clusteraccess_managed_environment_id:    managedEnvironment.Managedenvironment_id,
-		Clusteraccess_gitops_engine_instance_id: gitopsEngineInstance.Gitopsengineinstance_id,
-	}
+			clusterAccess := db.ClusterAccess{
+				Clusteraccess_user_id:                   clusterUser.Clusteruser_id,
+				Clusteraccess_managed_environment_id:    managedEnvironment.Managedenvironment_id,
+				Clusteraccess_gitops_engine_instance_id: gitopsEngineInstance.Gitopsengineinstance_id,
+			}
 
-	err = dbq.CreateClusterCredentials(ctx, &clusterCredentials)
-	if !assert.NoError(t, err) {
-		return
-	}
+			err = dbq.CreateClusterCredentials(ctx, &clusterCredentials)
+			Expect(err).To(BeNil())
 
-	err = dbq.CreateManagedEnvironment(ctx, &managedEnvironment)
-	if !assert.NoError(t, err) {
-		return
-	}
+			err = dbq.CreateManagedEnvironment(ctx, &managedEnvironment)
+			Expect(err).To(BeNil())
 
-	err = dbq.CreateGitopsEngineCluster(ctx, &gitopsEngineCluster)
-	if !assert.NoError(t, err) {
-		return
-	}
+			err = dbq.CreateGitopsEngineCluster(ctx, &gitopsEngineCluster)
+			Expect(err).To(BeNil())
 
-	err = dbq.CreateGitopsEngineInstance(ctx, &gitopsEngineInstance)
-	if !assert.NoError(t, err) {
-		return
-	}
+			err = dbq.CreateGitopsEngineInstance(ctx, &gitopsEngineInstance)
+			Expect(err).To(BeNil())
 
-	err = dbq.CreateClusterAccess(ctx, &clusterAccess)
-	if !assert.NoError(t, err) {
-		return
-	}
-	fetchRow := &ClusterAccess{Clusteraccess_user_id: clusterAccess.Clusteraccess_user_id,
-		Clusteraccess_managed_environment_id:    clusterAccess.Clusteraccess_managed_environment_id,
-		Clusteraccess_gitops_engine_instance_id: clusterAccess.Clusteraccess_gitops_engine_instance_id}
-	err = dbq.GetClusterAccessByPrimaryKey(ctx, fetchRow)
-	assert.NoError(t, err)
-	assert.ObjectsAreEqualValues(fetchRow, clusterAccess)
+			err = dbq.CreateClusterAccess(ctx, &clusterAccess)
+			Expect(err).To(BeNil())
+			fetchRow := db.ClusterAccess{Clusteraccess_user_id: clusterAccess.Clusteraccess_user_id,
+				Clusteraccess_managed_environment_id:    clusterAccess.Clusteraccess_managed_environment_id,
+				Clusteraccess_gitops_engine_instance_id: clusterAccess.Clusteraccess_gitops_engine_instance_id}
+			err = dbq.GetClusterAccessByPrimaryKey(ctx, &fetchRow)
+			Expect(err).To(BeNil())
+			Expect(fetchRow).Should(Equal(clusterAccess))
 
-	affectedRows, err := dbq.DeleteClusterAccessById(ctx, fetchRow.Clusteraccess_user_id, fetchRow.Clusteraccess_managed_environment_id, fetchRow.Clusteraccess_gitops_engine_instance_id)
-	assert.NoError(t, err)
-	assert.True(t, affectedRows == 1)
+			affectedRows, err := dbq.DeleteClusterAccessById(ctx, fetchRow.Clusteraccess_user_id, fetchRow.Clusteraccess_managed_environment_id, fetchRow.Clusteraccess_gitops_engine_instance_id)
+			Expect(err).To(BeNil())
+			Expect(affectedRows).To(Equal(1))
 
-	err = dbq.GetClusterAccessByPrimaryKey(ctx, fetchRow)
-	assert.True(t, IsResultNotFoundError(err))
-}
+			err = dbq.GetClusterAccessByPrimaryKey(ctx, &fetchRow)
+			Expect(db.IsResultNotFoundError(err)).To(Equal(true))
+		})
+	})
+})
